8e00f03b02c5a36055dfe508db2c79f17c30ac13
雷军 <xiaomi@163.com>
2020-09-29 12:42:33 +0800

    替换user

--- meiduo/meiduo/settings.py
+++ meiduo/meiduo/settings.py
@@ -184,3 +184,5 @@ LOGGING = {
         },
     }
 }
+#
+AUTH_USER_MODEL = 'users.User'
--- meiduo/meiduo/urls.py
+++ meiduo/meiduo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 def log(request):
 
@@ -31,4 +31,5 @@ def log(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('log/',log),
+    # path('',include('apps.users.urls'))
 ]
