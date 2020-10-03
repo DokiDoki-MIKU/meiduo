4bccdef4b471af28e3881ff5baa379ab9b90ff5e
雷军 <xiaomi@163.com>
2020-10-03 13:32:39 +0800

    替换模型

--- meiduo_02/meiduo_02/settings.py
+++ meiduo_02/meiduo_02/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -182,3 +183,5 @@ LOGGING = {
         },
     }
 }
+
+AUTH_USER_MODEL = 'users.User'
