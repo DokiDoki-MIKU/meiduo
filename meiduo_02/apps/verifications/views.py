829c66f7880f8092b6900b49805093a4718dfc3a
雷军 <xiaomi@163.com>
2020-10-04 12:18:28 +0800

    注册短信发送验证码

--- meiduo_02/apps/verifications/views.py
+++ meiduo_02/apps/verifications/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -15,3 +15,26 @@ class ImageCodeView(View):
         redis_cli.setex(uuid,100,text)
 
         return HttpResponse(image,content_type='image/jpeg')
+class SmscCodeView(View):
+    def get(self,request,mobile):
+        image_code=request.GET.get('image_code')
+        uuid=request.GET.get('image_code_id')
+
+        if not all([image_code,uuid]):
+            return JsonResponse({'code':400,'errmsg':'参数不全'})
+
+        from  django_redis import get_redis_connection
+        redis_cli=get_redis_connection('code')
+
+        redis_image_code=redis_cli.get(uuid)
+        if redis_image_code is None:
+            return JsonResponse({'code':400,'errmsg':'验证码已过期'})
+        if redis_image_code != image_code:
+            return JsonResponse({'code':400,'errmsg':'图片验证码错误'})
+        from random import randint
+        sms_code='%06d'%randint(0,999999)
+        redis_cli.setex(mobile,300,sms_code)
+        from libs.yuntongxun.sms import CCP
+        CCP().send_template_sms(mobile,[sms_code,5],1)
+
+        return  JsonResponse({'code':0,'errmsg':'ok!'})
