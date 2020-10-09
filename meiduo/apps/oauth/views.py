d9e38a6f38944820ff2bb37a30075a725c48e32d
雷军 <xiaomi@163.com>
2020-10-09 13:59:23 +0800

    绑定用户

--- meiduo/apps/oauth/views.py
+++ meiduo/apps/oauth/views.py
@@ -43,8 +43,48 @@ class OauthQQView(View):
 
             response.set_cookie('username',qquser.user.username)
 
+from apps.oauth.models import OAuthQQUser
+from django.contrib.auth import login
+from apps.users.models import User
+import json
+import re
+class OautQQView(View):
+    def get(self,request):...
 
-            pass
+    def post(self,request):
+        data = json.loads(request.body.decode())
+
+        mobile=data.get('mobile')
+        password=data.get('password')
+        sms_code=data.get('sms_code')
+        openid=data.get('access_token')
+        if not all([mobile, password, sms_code]):
+            return JsonResponse({'code': 400,'errmsg': '缺少必传参数'})
+
+            # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400,'errmsg': '请输入正确的手机号码'})
+
+            # 判断密码是否合格
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400,'errmsg': '请输入8-20位的密码'})
+
+        try:
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+
+            user=User.objects.create_user(username=mobile,mobile=mobile,password=password)
+
+        else:
+            if not user.check_password(password):
+                return JsonResponse({'code':400,'errmsg':'账号或密码错误'})
+        OAuthQQUser.objects.create(user=user,openid=openid)
+
+        login(request,user)
+
+        response=JsonResponse({'code':0,'errmsg':'ok'})
+        response.set_cookie('username',user.username)
+        return response
 
 
 
