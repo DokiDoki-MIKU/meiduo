d05f1cb8e06a74baf0def4cf0e98d41109359b1d
雷军 <xiaomi@163.com>
2020-10-10 19:40:58 +0800

    修复登录ＢＵＧ

--- meiduo/apps/users/urls.py
+++ meiduo/apps/users/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('usernames/<username>/count/',UsernameCountView.as_view()),
     path('register/',RegisterView.as_view()),
     path('mobiles/<mobile>/count/',MobileCountView.as_view()),
-    path('login/',LoginView.as_view),
-    path('logout/',LogoutView.as_view),
-    path('center/',LogoutView.as_view)
+    path('login/',LoginView.as_view()),
+    path('logout/',LogoutView.as_view()),
+    path('center/',LogoutView.as_view())
 
 ]
