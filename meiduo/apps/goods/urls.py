03a2e84916466b24184d721d2197f4e1038b4346
雷军 <xiaomi@163.com>
2020-10-16 20:58:46 +0800

    页面静态化

--- meiduo/apps/goods/urls.py
+++ meiduo/apps/goods/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('index/',IndexView.as_view()),
     path('list/<category_id>/skus/',ListView.as_view()),
     path('search/',SKUSearchchView()),
-    path('detail/<sku_id>/',DetailView()),
+    path('detail/<sku_id>/',DetailView.as_view()),
 ]
 
